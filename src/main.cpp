252c0dbda08c2b6dbc01d901b5be6dc641b201f8
Wilhan Almeida <wilhan.amello@gmail.com>
2026-06-17 19:37:05 -0300

    Testendo a tela colorida SPI

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,26 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <TFT_eSPI.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
-// VARIÁVEIS DE MOVIMENTO
-int quadradoX = 0;     // Posição horizontal inicial do quadrado
-int velocidadeX = 15;   // Quantos pixels ele anda por frame (Velocidade)
-int direcao = 1;       // 1 significa Direita, -1 significa Esquerda
+TFT_eSPI tft = TFT_eSPI();
 
 void setup() {
-  Wire.begin(13, 14);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    for(;;);
-  }
+
+  tft.init(); //inicaliza a tela
+  tft.setRotation(3); // seta a orientação da tela entra as opções de 0 a 3
+  uint16_t corFundo = tft.color565(10,30,60); //uma cor para o fundo deste exemplo
+  tft.fillScreen(corFundo); //seta o fundo para acor criada
+
+  // (X, Y, Largura, Altura, Cor)
+  tft.drawRect(60,44,40,40,TFT_WHITE);
+
+  // config do texto
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW, corFundo);
+  tft.setCursor(45,95);
+  tft.print("TESTE 1: OK")
+
 }
 
 void loop() {
-  // PASSO 2: Limpa o frame anterior
-  display.clearDisplay();
 
-  // PASSO 3: Desenha o quadrado na posição atual da variável 'quadradoX'
-  // fillRect(X, Y, Largura, Altura, Cor)
-  display.fillRect(quadradoX, 27, 10, 10, SSD1306_WHITE);
-
-  // PASSO 4: Envia para a tela e aguarda um pouco (~30 frames por segundo)
-  display.display();
-  delay(30); 
-
-  // PASSO 1: Atualiza a posição para o próximo frame
-  quadradoX = quadradoX + (velocidadeX * direcao);
-
-  // LOGICA DE COLISÃO: Bateu nas bordas?
-  // O quadrado tem 10px de largura, então na direita ele bate em 128 - 10 = 118
-  if (quadradoX >= 118) {
-    direcao = -1; // Inverte para ir para a esquerda
-  }
-  if (quadradoX <= 0) {
-    direcao = 1;  // Inverte para ir para a direita
-  }
 }
