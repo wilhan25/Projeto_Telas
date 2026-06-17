ebaa3f8ad625ad8d756f4fc86f733fa06aff9e09
Wilhan Almeida <wilhan.amello@gmail.com>
2026-06-17 20:18:58 -0300

    usando millis ao inves de delay para ter 2 objetos em tempos diferentes atualizando na tela ao mesmo tempo

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 #include <TFT_eSPI.h>
 
 TFT_eSPI tft = TFT_eSPI();
-int bolaRaio = 8, bolaX,bolaY, velocX = 2,velocY = 2;
+int bolaRaio = 8, bolaX,bolaY, velocX = 3,velocY = 3;
+
+unsigned long tempoAnteriorBola = 0;
+const long intervaloBola = 16;
+
+unsigned long tempoAnteriorTexto = 0;
+const long intervaloTexto = 500;
+bool mostrarTexto = true;
 
 void setup() {
 
@@ -17,22 +24,40 @@ void setup() {
 
 void loop() {
 
-  tft.fillCircle(bolaX,bolaY,bolaRaio, TFT_BLACK); //apaga a bolinha anterior
+  unsigned long tempoAtual = millis();
 
-  bolaX += velocX;
-  bolaY += velocY;
-  
-  // Se a posição X da bola passar da largura da tela (menos o raio) ou for menor que o raio:
-  if(bolaX >= (tft.width()- bolaRaio) || bolaX<= bolaRaio){
-    velocX = -velocX;
+  if(tempoAtual - tempoAnteriorBola >= intervaloBola){
+    tempoAnteriorBola = tempoAtual;
+
+    tft.fillCircle(bolaX,bolaY,bolaRaio,TFT_BLACK);
+
+    bolaX += velocX;
+    bolaY += velocY;
+    if(bolaX >= (tft.width() - bolaRaio) || bolaX <=bolaRaio){
+      velocX = -velocX;
+    }
+    if(bolaY>= (tft.height() - bolaRaio) || bolaY <= bolaRaio){
+      velocY = -velocY;
+    }
+
+    tft.fillCircle(bolaX,bolaY,bolaRaio, TFT_GREEN);
   }
-  // Colisão no Topo ou no Fundo
-  if(bolaY >=(tft.height()-bolaRaio) || bolaY <= bolaRaio){
-    velocY = -velocY;
+
+  if(tempoAtual - tempoAnteriorTexto >= intervaloTexto){
+    tempoAnteriorTexto = tempoAtual;
+
+    mostrarTexto = !mostrarTexto;
+
+    tft.setTextSize(1);
+    tft.setTextDatum(BC_DATUM); //Alinhamento no centro inferior da tela
+
+    if(mostrarTexto){
+      tft.setTextColor(TFT_RED, TFT_BLACK);
+      tft.drawString("SISTEMA ATIVO", tft.width()/2, tft.height()-10);
+    }else {
+      // Desenha o texto com a cor preta para "esconder" ele
+      tft.setTextColor(TFT_BLACK, TFT_BLACK);
+      tft.drawString("SISTEMA ATIVO", tft.width() / 2, tft.height() - 10);
+    }
   }
-
-  tft.fillCircle(bolaX,bolaY,bolaRaio,TFT_GREEN); //desenha a bolinha
-
-  delay(16); //delay para ficar a 60fps
-
 }
