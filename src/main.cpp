98f197f02d56b3d074dfac9bf305bebf5736605b
Wilhan Almeida <wilhan.amello@gmail.com>
2026-06-18 08:48:24 -0300

    menu com navegação pelo monitor serial

--- src/main.cpp
+++ src/main.cpp
@@ -2,62 +2,73 @@
 #include <TFT_eSPI.h>
 
 TFT_eSPI tft = TFT_eSPI();
-int bolaRaio = 8, bolaX,bolaY, velocX = 3,velocY = 3;
+const char* menuItens[] = {"1. STATUS","2. CONFIGS", "3. SENSORES", "4. SOBRE"};
+const int TOTAL_ITENS = 4;
 
-unsigned long tempoAnteriorBola = 0;
-const long intervaloBola = 16;
+int itemSelecionado = 0;
+bool atualizarTela = true;
 
-unsigned long tempoAnteriorTexto = 0;
-const long intervaloTexto = 500;
-bool mostrarTexto = true;
+void desenharMenu(){
+  tft.fillScreen(TFT_BLACK); //pinta a tela toda de preto para limpar e desenhar o proximo quadro
+
+  // cabeçalho
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW);
+  tft.setTextDatum(TC_DATUM);
+  tft.drawString("MENU PRINCIPAL", tft.width()/2,6);
+  tft.drawFastHLine(10,18,tft.width()-20, TFT_YELLOW);
+
+  // corpo do menu
+  tft.setTextDatum(ML_DATUM);
+  for(int i = 0; i< TOTAL_ITENS; i++){
+    int posY = 38 + (i*20);
+
+    if(i == itemSelecionado){
+      tft.fillRect(10, posY - 8, tft.width()-20,16,TFT_BLUE);
+      tft.setTextColor(TFT_WHITE, TFT_BLUE);
+    }
+    else{
+      tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    }
+    
+    tft.drawString(menuItens[i], 20,posY);
+  }
+}
 
 void setup() {
+  Serial.begin(115200); // Inicializa a comunicação com o computador
+  vTaskDelay(pdMS_TO_TICKS(500));
+  
+  tft.init();
+  tft.setRotation(3); 
+  tft.fillScreen(TFT_BLACK);
 
-  tft.init(); //inicaliza a tela
-  tft.setRotation(3); // seta a orientação da tela entra as opções de 0 a 3
-  tft.fillScreen(TFT_BLACK); //seta o fundo para preto
-
-  // calculo pra ficar no meio da tela no inicio
-  bolaX = tft.width()/2; 
-  bolaY = tft.height()/2;
+  Serial.println("Menu Inciado Use W e S para navegar");  
 }
 
 void loop() {
-
-  unsigned long tempoAtual = millis();
-
-  if(tempoAtual - tempoAnteriorBola >= intervaloBola){
-    tempoAnteriorBola = tempoAtual;
-
-    tft.fillCircle(bolaX,bolaY,bolaRaio,TFT_BLACK);
-
-    bolaX += velocX;
-    bolaY += velocY;
-    if(bolaX >= (tft.width() - bolaRaio) || bolaX <=bolaRaio){
-      velocX = -velocX;
-    }
-    if(bolaY>= (tft.height() - bolaRaio) || bolaY <= bolaRaio){
-      velocY = -velocY;
-    }
-
-    tft.fillCircle(bolaX,bolaY,bolaRaio, TFT_GREEN);
+  if(atualizarTela){
+    desenharMenu();
+    atualizarTela = false;
   }
 
-  if(tempoAtual - tempoAnteriorTexto >= intervaloTexto){
-    tempoAnteriorTexto = tempoAtual;
+  if(Serial.available() > 0){
+    char comando = Serial.read();
 
-    mostrarTexto = !mostrarTexto;
+    if(comando == 's' || comando =='S'){
+      itemSelecionado++;
+      if(itemSelecionado >= TOTAL_ITENS){
+        itemSelecionado = 0;
+      }
+      atualizarTela = true;
+    }
 
-    tft.setTextSize(1);
-    tft.setTextDatum(BC_DATUM); //Alinhamento no centro inferior da tela
-
-    if(mostrarTexto){
-      tft.setTextColor(TFT_RED, TFT_BLACK);
-      tft.drawString("SISTEMA ATIVO", tft.width()/2, tft.height()-10);
-    }else {
-      // Desenha o texto com a cor preta para "esconder" ele
-      tft.setTextColor(TFT_BLACK, TFT_BLACK);
-      tft.drawString("SISTEMA ATIVO", tft.width() / 2, tft.height() - 10);
+    if(comando == 'w' || comando =='W'){
+      itemSelecionado--;
+      if(itemSelecionado < 0){
+        itemSelecionado = TOTAL_ITENS -1;
+      }
+      atualizarTela = true;
     }
   }
 }
